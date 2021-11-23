9bd6d6b9e8de7bfa1baa488cb548ed9a008ff9e5
Zhang_abab <2411358676@qq.com>
2021-11-23 11:46:59 +0800

    飞船运动

--- alinen_invasion.py
+++ alinen_invasion.py
@@ -24,6 +24,9 @@ class AlienInvasion:
     def run_game(self):
         '''开始游戏'''
         while True:
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
             #监视键盘和鼠标事件
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -34,6 +37,30 @@ class AlienInvasion:
 
             # 让最近绘制的屏幕可见。 
             pygame.display.flip()
+
+    def _check_events(self):
+        """响应按键和鼠标事件"""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                if event.key == pygame.K_LEFT:
+                    self.ship.moving_left = False
+    
+    def _update_screen(self):
+        """更新屏幕上的图像，并切换到新的屏幕"""
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+        pygame.display.flip()
 
 if __name__ == '__main__':
     #创建游戏实例并运行游戏
